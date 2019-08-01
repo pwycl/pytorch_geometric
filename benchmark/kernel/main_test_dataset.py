8e978a2ed3719b0d0938de03a864165653584473
pwycl <814821479@qq.com>
2019-08-02 06:52:52 +0800

    Update main_test_dataset.py

--- benchmark/kernel/main_test_dataset.py
+++ benchmark/kernel/main_test_dataset.py
@@ -26,7 +26,7 @@ parser.add_argument('--dataset',default='MUTAG')
 parser.add_argument('--dataset_div',type=int, default=None)
 args = parser.parse_args()
 
-layers = [3, 4, 5]
+layers = [1, 2,]
 hiddens = [16,]#32, 64, 128]
 
 #layers=[1, 2, 3, 4,5]
