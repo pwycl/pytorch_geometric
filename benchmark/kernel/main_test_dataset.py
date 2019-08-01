9334ac719bca912d95ef585f80f9944f9e302b5f
pwycl <814821479@qq.com>
2019-08-01 18:07:18 +0800

    Update main_test_dataset.py

--- benchmark/kernel/main_test_dataset.py
+++ benchmark/kernel/main_test_dataset.py
@@ -26,10 +26,10 @@ parser.add_argument('--dataset',default='MUTAG')
 parser.add_argument('--dataset_div',type=int, default=None)
 args = parser.parse_args()
 
-layers = [3 ]#, 4, 5]
+layers = [4, ]#5]
 hiddens = [32, 64, 128]
 
-#layers=[1, 2,]
+#layers=[1, 2, 3]
 #hiddens = [16, ]
 # datasets = ['MUTAG', 'PROTEINS', 'IMDB-BINARY', 'REDDIT-BINARY','ENZYMES']  # , 'COLLAB']
 datasets=[args.dataset]
