ef7b1add2bb5a36a3a68cae7639c42000f629cac
pwycl <814821479@qq.com>
2019-09-07 19:48:38 +0800

    Update txt_array.py

--- torch_geometric/read/txt_array.py
+++ torch_geometric/read/txt_array.py
@@ -9,5 +9,5 @@ def parse_txt_array(src, sep=None, start=0, end=None, dtype=None, device=None):
 
 def read_txt_array(path, sep=None, start=0, end=None, dtype=None, device=None):
     with open(path, 'r') as f:
-        src = f.read().split('\n')[:-1]
+        src = f.readlines()
     return parse_txt_array(src, sep, start, end, dtype, device)
